ec07a088ce7c67b1363ecf6fde652e7eae747473
Rachael Peri <raheraperi@Rachaels-MacBook-Air.local>
2018-10-26 15:47:06 +1300

    compiled smart contract

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,12 +1,28 @@
 pragma solidity ^0.4.2; 
 
 contract Election {
-  //Store candidate 
 
+  //Model a Candidate 
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+  //Store candidates
+  //Fetch candidates
+    mapping(uint => Candidate) public candidates; 
+  //Store Candidates Count 
+    uint public candidatesCount; 
   //Read candidate
-    string public candidate;
+    // string public candidate;
   //Constructor
     function Election () public {
-        candidate = "Candidate 1";
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate (string _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 }
